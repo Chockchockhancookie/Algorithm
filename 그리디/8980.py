990c25c4afe19a96d556233647492b1a293ea579
CHOCOChip_93 <mellow3632@naver.com>
2021-08-02 22:10:34 +0900

    0820

--- 그리디/8980.py
+++ 그리디/8980.py
@@ -5,10 +5,34 @@ m = int(input())
 
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append([b, c])
+    a, b, cost = map(int, input().split())
+    graph[a].append([b, cost])
+
+print(graph)
 
 num = 0
+answer = 0
 queue = []
 for i in range(1, n+1):
-    
+    while queue:
+        now, dist = heapq.heappop(queue)
+        if now == i:
+            answer += dist
+            num -= dist
+        else:
+            heapq.heappush(queue, (now, dist))
+            break
+
+    for node, weight in graph[i]:
+        tmp = c - num
+        if tmp > 0:
+            if tmp > weight:
+                num += weight
+
+                heapq.heappush( queue, (node, weight))
+            else:
+                num += tmp
+                heapq.heappush(queue, (node, tmp
+                                       ))
+
+print(answer)
