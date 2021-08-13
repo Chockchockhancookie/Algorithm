55e5a37e635617470c55662b2353801d67fb6440
CHOCOChip_93 <mellow3632@naver.com>
2021-08-13 23:33:47 +0900

    0813

--- random_exam.py
+++ random_exam.py
@@ -1,14 +1,31 @@
-n, m = map(int, input().split())
-data = [list(map(int, input().split())) for _ in range(n)]
+from collections import deque
 
-for a in range(n):
-    for b in range(m):
-        print(data[a][b], end=" ")
-    print()
-print()
+a, b = map(int, input().split())
+MAX = 10000
+counts = [0] * (MAX + 1)
 
-array = data[:]
-for a in range(n):
-    for b in range(m):
-        print(array[a][b], end=" ")
-    print()
+
+def sosoo(x):
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+
+counts[a] = 0
+q = deque()
+q.append(a)
+while q:
+    x = q.popleft()
+    if x == b: break
+
+    for i in range(4):
+        A = list(map(int, str(x)))
+        for j in range(1, 10):
+            if A[i] == j: continue
+            A[i] = j
+            B = (A[0] * 1000) + (A[1] * 100) + (A[2] * 10) + A[3]
+            if 1000 < B < MAX and sosoo(B) and not counts[B]:
+                counts[B] = counts[x] + 1
+                q.append(B)
+print(counts[b])
