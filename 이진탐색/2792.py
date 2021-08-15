5fff9e362e3d77e54e5d6076a40eeb3f66dd4eff
CHOCOChip_93 <mellow3632@naver.com>
2021-08-15 23:15:38 +0900

    confirm

--- 이진탐색/2792.py
+++ 이진탐색/2792.py
@@ -1,6 +1,23 @@
-n, m = map(int, input().split())    # 아이들의 수, 색상의 수
-color = list(int(input()) for _ in range(m))
+import math
+import sys
+input = sys.stdin.readline
 
-color.sort(reverse=True)
+n, m = map(int, input().split())
+data = [int(input()) for _ in range(m)]
 
+start, end = 1, max(data)
 
+answer = int(1e9)
+while start <= end:
+    count = 0
+    mid = (start+end) // 2
+    for jew in data:
+        count += math.ceil(jew/mid)
+
+    if count > n:
+        start = mid+1
+    else:
+        end = mid-1
+        answer = min(answer, mid)
+
+print(answer)
